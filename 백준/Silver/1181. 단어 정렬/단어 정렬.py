4fcdee176b350ea1f70d0e4691c64a6989d12425
ajosd10-afk <ajosd10@gmail.com>
2026-02-02 15:01:09 +0900

    [Silver V] Title: 단어 정렬, Time: 480 ms, Memory: 37024 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,11 @@
+n = int(input()) # 단어 개수 받아오기
+
+input_list = [] # 입력된 값을 받을 빈 리스트
+for j in range(n):
+    input_list.append(str(input())) # 입력된 단어 리스트
+
+input_list = list(set(input_list))
+sorted_list = sorted(input_list, key=lambda x: (len(x),x)) # 길이와 사전순으로 정렬
+
+for i in sorted_list: # 하나씩 뽑아와서 프린트
+    print(i)
